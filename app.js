be7ceb340b2de7787b8930bb2184709e3e1932ff
Pooja Rani <pooja.rani@metropolia.fii>
2024-04-07 01:35:30 +0530

    first commit

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const methodOverride = require('method-override')
 
 const connectDB = require("./config/db");
 
-const blogAPI = require("./controllers/inventoryAPIController");
-const blogSSR = require("./controllers/inventorySSRController");
+const blogAPI = require("./controllers/contactAPIController");
+const blogSSR = require("./controllers/contactSSRController");
 
 //Important: will be discussed next week
 app.use(express.json());
@@ -24,34 +24,34 @@ app.use(express.static("public"));
 connectDB();
 
 // SSR
-// Route to render index.html with inventorys using EJS
-app.get("/", blogSSR.renderInventorys);
+// Route to render index.html with contacts using EJS
+app.get("/", blogSSR.renderContacts);
 // Define a route to render the addgoal.ejs view
 app.get("/addgoal", blogSSR.renderForm);
-// Route to add  inventory using EJ
-app.post("/addgoal", blogSSR.addInventory);
+// Route to add  contact using EJ
+app.post("/addgoal", blogSSR.addContact);
 // Define a route to render the singlegoal.ejs view
-app.get("/single-inventory/:id", blogSSR.renderInventory);
+app.get("/single-contact/:id", blogSSR.renderContact);
 // Define a route to delete singlegoal
-app.delete("/single-inventory/:id", blogSSR.deleteInventory);
-// Define a route to update single inventory.ejs
-app.put("/single-inventory/:id", blogSSR.updateInventory);
-// Define inventory to update
-app.get("/single-inventory/update/:id", blogSSR.renderUpdateInventory);
+app.delete("/single-contact/:id", blogSSR.deleteContact);
+// Define a route to update single contact.ejs
+app.put("/single-contact/:id", blogSSR.updateContact);
+// Define contact to update
+app.get("/single-contact/update/:id", blogSSR.renderUpdateContact);
 
 // API
-// GET all Inventorys
-app.get("/api/inventorys", blogAPI.getInventorys);
-// POST a new Inventory
-app.post("/api/inventorys", blogAPI.addInventory);
-// GET a single Inventory
-app.get("/api/inventorys/:id", blogAPI.getInventory);
-// Update Inventory using PUT
-app.put("/api/inventorys/:id", blogAPI.updateInventory);
-// DELETE a Inventory
-app.delete("/api/inventorys/:id", blogAPI.deleteInventory);
-// DELETE all Inventory
-app.delete("/api/inventorys", blogAPI.deleteAllInventorys);
+// GET all Contacts
+app.get("/api/contacts", blogAPI.getContacts);
+// POST a new Contact
+app.post("/api/contacts", blogAPI.addContact);
+// GET a single Contact
+app.get("/api/contacts/:id", blogAPI.getContact);
+// Update Contact using PUT
+app.put("/api/contacts/:id", blogAPI.updateContact);
+// DELETE a Contact
+app.delete("/api/contacts/:id", blogAPI.deleteContact);
+// DELETE all Contact
+app.delete("/api/contacts", blogAPI.deleteAllContacts);
 
 const PORT = 4000;
 
